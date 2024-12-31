15cb2d26af38bb4309df519d4ddface38ad466f8
Andras Fejes <afejes@liveperson.com>
2024-12-31 13:40:53 +0100

    📝Described configuration properties

--- quarkus-quinoa/src/main/webui/orval.config.ts
+++ quarkus-quinoa/src/main/webui/orval.config.ts
@@ -2,12 +2,18 @@ import { defineConfig } from 'orval';
 
 export default defineConfig({
   catalog: {
+    /** The path of the generated OpenAPI scheme */
     input: 'api/catalog.yaml',
     output: {
+      /** Generates the scheme and client implementation in different files */
       mode: 'split',
+      /**  Path to the folder where you want to generate all your models. */
       schemas: 'src/app/api/model',
+      /** Path to the file which will contains the client implementation. */
       target: 'src/app/api/service/catalog.ts',
+      /** Type of the client that we use */
       client: 'angular',
+      /** Base url of the API; it is generated to the client implementation. It should be configuration value. */
       baseUrl: 'http://localhost:8080'
     },
     hooks: {
