734d4b33345a53c99c564b1c003d6c07de8f4aa9
Andras Fejes <afejes@liveperson.com>
2025-01-02 08:53:19 +0100

    🔊Added service level logging

--- quarkus-primefaces/src/main/java/io/crunch/catalog/ProductService.java
+++ quarkus-primefaces/src/main/java/io/crunch/catalog/ProductService.java
@@ -1,11 +1,16 @@
 package io.crunch.catalog;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.lang.invoke.MethodHandles;
 import java.util.List;
 
 @ApplicationScoped
 public class ProductService {
+
+    private final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     private final ProductRepository productRepository;
 
@@ -14,6 +19,7 @@ public class ProductService {
     }
 
     public List<Product> getProducts() {
+        logger.info("Getting all products");
         return productRepository.listAll();
     }
 }
